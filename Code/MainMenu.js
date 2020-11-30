17a09a273ad02e1b371a9c31d723390232bd2724
zngjacky <zng.jacky@gmail.com>
2020-11-29 20:25:35 -0500

    static analysis

--- Code/MainMenu.js
+++ Code/MainMenu.js
@@ -1,87 +1,83 @@
-var script = document.createElement('script');
-var socket = io();
-script.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js";
+const script = document.createElement('script');
+const socket = io();
+script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 
-function MainMenu(button){
-    if(button == "C"){
-        createGame();   
-    }
-    else if(button == "J") {
-        window.location="Join.html";       
-    }
-    else if (button == "I"){
-        window.location="Instructions.html";
-    }
+function MainMenu(button) {
+  if (button == 'C') {
+    createGame();
+  } else if (button == 'J') {
+    window.location='Join.html';
+  } else if (button == 'I') {
+    window.location='Instructions.html';
+  }
 }
 
-function Join(){
-    var randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var generatedResult = '';
-    for ( var i = 0; i < 5; i++ ) {
-      generatedResult += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+function Join() {
+  const randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let generatedResult = '';
+  for ( let i = 0; i < 5; i++ ) {
+    generatedResult += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+  }
+
+  const RoomCode = document.getElementById('RoomCode').value;
+  socket.emit('join', RoomCode, generatedResult);
+
+
+  socket.on(generatedResult, function(message) {
+    if (message == 'connect') {
+      window.location = 'Game.html?roomCode='+RoomCode;
+    } else {
+      alert(message);
     }
-
-    var RoomCode = document.getElementById("RoomCode").value;
-    socket.emit('join',RoomCode, generatedResult);
-    
-
-    socket.on(generatedResult, function (message) {
-        if (message == "connect"){
-            window.location = "Game.html?roomCode="+RoomCode;
-        }
-        else{
-            alert(message);
-        }
-    });
+  });
 }
 
 function createGame() {
-    var randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        var generatedResult = '';
-        for ( var i = 0; i < 5; i++ ) {
-          generatedResult += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
-        }
-    console.log(generatedResult)
-    socket.emit('join',generatedResult);
-    window.location="Game.html?roomCode="+generatedResult;
+  const randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let generatedResult = '';
+  for ( let i = 0; i < 5; i++ ) {
+    generatedResult += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+  }
+  console.log(generatedResult);
+  socket.emit('join', generatedResult);
+  window.location='Game.html?roomCode='+generatedResult;
 }
 
-function declareWinner(){
-    let URL = window.location.search;
-    let Params = new URLSearchParams(URL);
-    let W = Params.get('Winner')
-    console.log(W);
+function declareWinner() {
+  const URL = window.location.search;
+  const Params = new URLSearchParams(URL);
+  const W = Params.get('Winner');
+  console.log(W);
 
-    if (W == "Red") {
-        var String = "<mark class='red'><u><b>" + W + "</b></u></mark> Wins.";
-    } else if (W == "Black") {
-        var String = "<u><b>" + W + "</b></u> Wins";
-    }
-    document.getElementById("Winner").innerHTML = String
+  if (W == 'Red') {
+    const String = '<mark class=\'red\'><u><b>' + W + '</b></u></mark> Wins.';
+  } else if (W == 'Black') {
+    const String = '<u><b>' + W + '</b></u> Wins';
+  }
+  document.getElementById('Winner').innerHTML = String;
 }
 
-function Rematch(){
-    var randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var generatedResult = '';
-    for ( var i = 0; i < 5; i++ ) {
-      generatedResult += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+function Rematch() {
+  const randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let generatedResult = '';
+  for ( let i = 0; i < 5; i++ ) {
+    generatedResult += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+  }
+
+  const URL = window.location.search;
+  const Params = new URLSearchParams(URL);
+  const RoomCode = Params.get('roomCode');
+
+  socket.emit('join', RoomCode, generatedResult);
+
+  socket.on(generatedResult, function(message) {
+    if (message == 'connect') {
+      window.location = 'Game.html?roomCode='+RoomCode;
+    } else {
+      alert(message);
+      window.location = 'index.html';
     }
-
-    let URL = window.location.search;
-    let Params = new URLSearchParams(URL);
-    let RoomCode = Params.get('roomCode')
-
-    socket.emit('join', RoomCode, generatedResult);
-    
-    socket.on(generatedResult, function (message) {
-        if (message == "connect"){
-            window.location = "Game.html?roomCode="+RoomCode;
-        }
-        else{
-            alert(message);
-            window.location = "index.html";
-        }
-    });
+  });
 }
--- Code/gserver.js
+++ Code/gserver.js
@@ -1,16 +1,14 @@
-const { timeStamp } = require('console');
-var express = require('express');
-var app = express();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
-var sqlite3 = require('sqlite3').verbose();
-const fs = require('fs');
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const sqlite3 = require('sqlite3').verbose();
 
-let db = new sqlite3.Database("./game.db", (err) => {
-    if (err) {
-        return console.log(err.message);
-    }
-    console.log("Connected to the SQLite db");
+const db = new sqlite3.Database('./game.db', (err) => {
+  if (err) {
+    return console.log(err.message);
+  }
+  console.log('Connected to the SQLite db');
 });
 
 // let sql = `SELECT * FROM gameInfo`;
@@ -24,24 +22,15 @@ let db = new sqlite3.Database("./game.db", (err) => {
 //     });
 // });
 
-let update = `REPLACE INTO gameInfo(gameID, gameHTML, turnVar, checkVar) values (?,?,?,?)`
+const update = `REPLACE INTO gameInfo(gameID, gameHTML, turnVar, checkVar) values (?,?,?,?)`;
 
-let create = `INSERT INTO game (gameID) values (?)`;
+const create = `INSERT INTO game (gameID) values (?)`;
 
-app.use(express.static("."));
+app.use(express.static('.'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
-
-function getBoard(roomCode, board){
-    let tmp = [];
-    tmp.push(roomCode);
-    tmp.push(board[0]);
-    tmp.push(board[1]);
-    tmp.push(board[2]);
-    return tmp;
-}
 
 // function getTurnVar(roomCode){
 //     db.get(`SELECT turnVar FROM game WHERE gameID = (?)`, [roomCode], (err, row) =>{
@@ -56,90 +45,85 @@ function getBoard(roomCode, board){
 // }
 
 io.on('connection', (socket) => {
-    // console.log('a user connected');
-    // console.log(socket.id)
-    // socket.on('disconnect', () =>{
-    //     console.log('user disconnected');
-    // });
-    socket.on('gameID', (gameID) => {
-        db.run(create, [gameID], function(err){
-            if (err){
-                return console.log(err.message);
-            }
-            console.log(`A row has been inserted with this rowid ${this.lastID}`);
-        });
+  // console.log('a user connected');
+  // console.log(socket.id)
+  // socket.on('disconnect', () =>{
+  //     console.log('user disconnected');
+  // });
+  socket.on('gameID', (gameID) => {
+    db.run(create, [gameID], function(err) {
+      if (err) {
+        return console.log(err.message);
+      }
+      console.log(`A row has been inserted with this rowid ${this.lastID}`);
     });
-    socket.on('board', (board, roomCode) => {
-        console.log(roomCode);
-        db.run(update, [roomCode, board[0], board[1], board[2]], function(err){
-            if (err){
-                return console.log(err.message);
-            }
-            console.log(`A row has been updated with this rowid ${this.lastID}`);
-        });
-        io.in(roomCode).emit('board', board);
+  });
+  socket.on('board', (board, roomCode) => {
+    console.log(roomCode);
+    db.run(update, [roomCode, board[0], board[1], board[2]], function(err) {
+      if (err) {
+        return console.log(err.message);
+      }
+      console.log(`A row has been updated with this rowid ${this.lastID}`);
     });
-    // socket.on('globals', (globals, roomCode) => {
-    //     console.log(globals);
-    //     io.in(roomCode).emit('globals', globals);
-    // });
+    io.in(roomCode).emit('board', board);
+  });
+  // socket.on('globals', (globals, roomCode) => {
+  //     console.log(globals);
+  //     io.in(roomCode).emit('globals', globals);
+  // });
 
-    // socket.on('create', function(){
-    //     var randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    //     var generatedResult = '';
-    //     for ( var i = 0; i < 5; i++ ) {
-    //       generatedResult += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
-    //     }
-    //     console.log(generatedResult)
-    
-    //     //Store Room code in SQL
-    
-    //     socket.join(generatedResult);
-    //     console.log(socket.rooms);
-    // })
+  // socket.on('create', function(){
+  //     var randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  //     var generatedResult = '';
+  //     for ( var i = 0; i < 5; i++ ) {
+  //       generatedResult += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+  //     }
+  //     console.log(generatedResult)
 
-    socket.on('join', function(roomCode,reqCode){
-        var Room = io.sockets.adapter.rooms.get(roomCode)
-        if (((Room == null) || ((Room).size != 2))){
-            socket.join(roomCode)
-            console.log("Joined")
-            console.log(socket.rooms)
-            console.log(io.sockets.adapter.rooms);
-            io.emit(reqCode,"connect");
-        }
-        else{
-            console.log("Full")
-            io.emit(reqCode,"Room Full");
-        }
-    })
+  //     //Store Room code in SQL
 
-    socket.on("setPlayer",function(roomCode){
-        var Room = io.sockets.adapter.rooms.get(roomCode)
-        if (Room.player1 == null){
-            Room.player1 = socket.id
-        }
-        else if (Room.has(Room.player1) == false){
-            Room.player1 = socket.id
-        }
-        else if(Room.player2 == null){
-            Room.player2 = socket.id
-        }
-        else if (Room.has(Room.player2) == false){
-            Room.player2 = socket.id
-        }
-    })
+  //     socket.join(generatedResult);
+  //     console.log(socket.rooms);
+  // })
 
-    socket.on("getPlayer",function(roomCode, reqCode){
-        var Room = io.sockets.adapter.rooms.get(roomCode)
-        if (Room.player1 == socket.id){
-            console.log("Red")
-            io.emit(reqCode, "Red")
-        }
-        else if (Room.player2 == socket.id){
-            console.log("Black")
-            io.emit(reqCode, "Black")
-        }
-    })
+  socket.on('join', function(roomCode, reqCode) {
+    const Room = io.sockets.adapter.rooms.get(roomCode);
+    if (((Room == null) || ((Room).size != 2))) {
+      socket.join(roomCode);
+      console.log('Joined');
+      console.log(socket.rooms);
+      console.log(io.sockets.adapter.rooms);
+      io.emit(reqCode, 'connect');
+    } else {
+      console.log('Full');
+      io.emit(reqCode, 'Room Full');
+    }
+  });
+
+  socket.on('setPlayer', function(roomCode) {
+    const Room = io.sockets.adapter.rooms.get(roomCode);
+    if (Room.player1 == null) {
+      Room.player1 = socket.id;
+    } else if (Room.has(Room.player1) == false) {
+      Room.player1 = socket.id;
+    } else if (Room.player2 == null) {
+      Room.player2 = socket.id;
+    } else if (Room.has(Room.player2) == false) {
+      Room.player2 = socket.id;
+    }
+  });
+
+  socket.on('getPlayer', function(roomCode, reqCode) {
+    const Room = io.sockets.adapter.rooms.get(roomCode);
+    if (Room.player1 == socket.id) {
+      console.log('Red');
+      io.emit(reqCode, 'Red');
+    } else if (Room.player2 == socket.id) {
+      console.log('Black');
+      io.emit(reqCode, 'Black');
+    }
+  });
 });
 
 // app.get('/createGame', (req, res) => {
@@ -153,15 +137,15 @@ io.on('connection', (socket) => {
 //     if (!fs.existsSync(dir)){
 //         fs.mkdirSync(dir);
 //     }
-  
+
 //     fs.copyFile('./CleanGame/Game.html', './Sessions/'+generatedResult+"/Game.html", (err) => {
-//       if (err) 
+//       if (err)
 //           throw err;
 //       console.log('Game.html was copied');
 //     });
-  
+
 //     fs.copyFile('./CleanGame/Game.js', './Sessions/'+generatedResult+"/Game.js", (err) => {
-//       if (err) 
+//       if (err)
 //           throw err;
 //       console.log('Game.js was copied');
 //     });
@@ -170,5 +154,5 @@ io.on('connection', (socket) => {
 
 
 http.listen(3000, () => {
-    console.log('listening on *:3000');
+  console.log('listening on *:3000');
 });
